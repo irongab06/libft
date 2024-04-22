79c87fb6e3364487ca4686c60d1aea882269e925
irongab <..>
2024-04-22 22:32:43 +0200

    ajout 22 04 2024 le soir

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -11,32 +11,38 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <string.h>
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t	i;
 	size_t	j;
-	unsigned char *d = (unsigned char *)dest;
-	const unsigned char *c = (const unsigned char *)src;
+	unsigned char	d;
+	unsigned char	c;
+
+	d = (unsigned char *)dest;
+	c = (unsigned char *)src;
+
 
 	j = 0;
-	i = 0;
-	while (j <= n)
+	if ((d == NULL) || (c == NULL))
+		return(NULL);
+	while (j < n)
 	{
-		d[i + j] = c[j];
+		d[j] = c[j];
 		j++;
 	}
 	return (dest);
 }
-/*
+
 int	main(void)
 {
 	char	dest[21];
 	char	dest1[21];
 
-	ft_memcpy(dest, "je m'appel aurélien", 21);
-	printf("faux %s", dest);
-	memcpy(dest1, "je m'appel aurélien", 21);
+	ft_memcpy(dest, "", 21);
+	printf("faux %s\n", dest);
+	memcpy(dest1, "", 21);
 	printf("vrai %s", dest1);
+	return (0);
 
-}*/
+}

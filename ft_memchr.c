5058e70ad599634b912c89f9d09f0b04f2fd7f97
irongab <..>
2024-04-23 23:16:55 +0200

    ajout ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -3,14 +3,16 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	c1;
-	unsigned char	s1;
+	unsigned char	*s1;
+	int	i;
 	
+	i = 0;
 	s1 = (unsigned char*)s;
 	c1 = (unsigned char)c;
 	while (n > 0)
 	{
-		if (s1[i] = c)
-			return (s1[i]);
+		if (s1[i] == c1)
+			return (s1 + i);
 		else
 			n--;
 			i++;

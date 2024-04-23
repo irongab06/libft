9d5865beb0889f72d914c8e0646dfc803e1ace2e
Gabriel Cavalier <gacavali@c2r8p4.42nice.fr>
2024-04-23 11:59:43 +0200

    ajout strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,11 +6,21 @@
 /*   By: gacavali <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 11:39:42 by gacavali          #+#    #+#             */
-/*   Updated: 2024/04/23 11:49:42 by gacavali         ###   ########.fr       */
+/*   Updated: 2024/04/23 11:58:56 by gacavali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
+	size_t	i;
 	
+	i = 0;
+	if (
+	while (i < size - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0'
+	return (i);
 }

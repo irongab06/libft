833e176f645ca3f67a9a5328e170389576208015
Gabriel Cavalier <gacavali@c2r2p2.42nice.fr>
2024-04-26 11:35:29 +0200

    ajout strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: gacavali <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 09:47:03 by gacavali          #+#    #+#             */
-/*   Updated: 2024/04/26 09:47:31 by gacavali         ###   ########.fr       */
+/*   Updated: 2024/04/26 11:35:04 by gacavali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	new_s = NULL;
 	if (s1 == 0 && s2 == 0)
 		return (new_s = ft_strdup(""));
-	new_s = malloc(sizeof(char) * i + 1);
+	new_s = malloc(sizeof(char) * (i + 1));
 	if (new_s == NULL)
 		return (NULL);
 	i = 0;

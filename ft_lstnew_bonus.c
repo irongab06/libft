ab9344e65f99a5e65d52b87041531f9c09900336
irongab <..>
2024-04-29 07:34:37 +0200

    ajout matin

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -1,11 +1,13 @@
+#include "libft.h"
+
 t_list	*ft_lstnew(void *content)
 {
 	t_list	*element;
 
 	element = malloc(sizeof(*element));
-	if (element = NULL)
+	if (element == NULL)
 		return (NULL);
 	element -> content = content;
 	element -> next = NULL;
-	return (element)
+	return (element);
 }

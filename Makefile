e7cd3e81ec22d2ea61eb69700c7a79a4b81b4200
Gabriel Cavalier <gacavali@c1r8p3.42nice.fr>
2024-04-30 15:58:04 +0200

    ajoutmardi

--- Makefile
+++ Makefile
@@ -16,7 +16,7 @@ ft_isdigit.c ft_isascii.c ft_isprint.c \
 
 SRC_BONUS = ft_lstnew.c ft_lstadd_front.c \
  ft_lstsize.c ft_lstlast.c ft_lstadd_back.c \
- ft_lstdelone.c
+ ft_lstdelone.c ft_lstclear.c ft_lstiter.c
 
 OBJ = $(SRC:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gacavali <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 15:21:55 by gacavali          #+#    #+#             */
-/*   Updated: 2024/04/30 13:02:40 by gacavali         ###   ########.fr       */
+/*   Updated: 2024/04/30 15:30:13 by gacavali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,6 +80,8 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 //--------------------------------------------------------------
 

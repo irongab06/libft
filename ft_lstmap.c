f9e42df9afd92ef42f6cafc87c87f547695037c9
irongab <..>
2024-05-01 07:55:49 +0200

    map

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -2,19 +2,8 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	int	i;
 	t_list	**new_lst;
-	t_list	*temp;
-	
-	temp = lst;
-	new_lst = malloc(sizeof(t_list));
-	if (new_lst == NULL)
-		return (NULL);
-	while (lst)
-	{	
-		new_lst = f(lst->content);
-		new_lst = &lst->next;
-		lst = lst->next;
-	}
-	return (*new_lst);
+	t_list	*element;
+
+	element = ft_lstnew(new_lst->content);
 }
